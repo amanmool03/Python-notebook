0e8fe9f1f3eb29bfbc5119de6ea6d57760e953bf
Aman mool <amanmool384@gmail.com>
2021-02-18 13:41:32 +0545

    python notebook and codes

---
+++ python/day1.py
@@ -0,0 +1,68 @@
+# eg_int = 10
+# eg_complex = 2+3j
+
+# print(eg_int, eg_complex)
+# a = 10
+# print(a % 2)
+
+
+# def adder(n1, n2, n3):
+#     print("n1={},n2={},n3={}".format(n1, n2, n3))
+#     return n1+n2+n3
+
+
+# print(adder(n2=3, n1=4, n3=1))
+
+
+# def sum_d(n):
+#     if n == 0:
+#         return 0
+#     return n % 10 + sum_d(n//10)
+
+
+# print(sum_d(51))
+
+
+# def something():
+#     pass
+#     print("hi")
+
+
+# print(something())
+
+# def something():
+#     return None
+#     print("hi")
+
+
+# print(something())
+
+
+# def something(a):
+#     print(id(a))
+
+
+# a = 10
+# print(id(a))
+# print(something(a))
+
+
+# a = 10
+# b = a
+
+# print(id(a))
+# print(id(b))
+
+
+# def something(arg):
+#     arg.append(10)
+
+
+# x = [1, 4, 5]
+
+# something(x)
+
+# print(x)
+
+
+
---
+++ python/main.py
@@ -0,0 +1,7 @@
+# import calculator
+
+# print(calculator.adder(10, 20))
+
+from calculator import adder, sub
+
+print(adder(10, 20))
